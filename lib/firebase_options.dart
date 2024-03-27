a37d29d2ed380c3c55e8ee7c472306d66803f353
Elijah Okoroh <89285052+okorohelijah@users.noreply.github.com>
2024-03-27 06:54:22 -0500

    Update firebase_options.dart

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -44,8 +44,8 @@ class DefaultFirebaseOptions {
   }
 
   static const FirebaseOptions web = FirebaseOptions(
-    apiKey: 'AIzaSyBJdSi6k2b5R1jAGM4f19zuP_j5Joyg_Dw',
-    appId: '1:73653702134:web:465c1a341771c85aad0bd7',
+    apiKey: 'AIzaSyBJ9zuP_j5Joyg_Dw',
+    appId: 'AIzaSyBJ9zuP_j5Joyg_Dw',
     messagingSenderId: '73653702134',
     projectId: 'quickie-23ff0',
     authDomain: 'quickie-23ff0.firebaseapp.com',
@@ -54,16 +54,16 @@ class DefaultFirebaseOptions {
   );
 
   static const FirebaseOptions android = FirebaseOptions(
-    apiKey: 'AIzaSyBODNhy-nesi245QotRPzcbCHyN_S50vnY',
-    appId: '1:73653702134:android:5a7f8f49086a4b37ad0bd7',
+    apiKey: 'AIzaSyBJ9zuP_j5Joyg_Dw',
+    appId: 'AIzaSyBJ9zuP_j5Joyg_Dw',
     messagingSenderId: '73653702134',
     projectId: 'quickie-23ff0',
     storageBucket: 'quickie-23ff0.appspot.com',
   );
 
   static const FirebaseOptions ios = FirebaseOptions(
-    apiKey: 'AIzaSyB6uQuqKV3Pe-no9xvsl1R6h-0NmUwOMGQ',
-    appId: '1:73653702134:ios:6de6386f02722f23ad0bd7',
+    apiKey: 'AIzaSyBJ9zuP_j5Joyg_Dw',
+    appId: '1:AIzaSyBJ9zuP_j5Joyg_Dwd7',
     messagingSenderId: '73653702134',
     projectId: 'quickie-23ff0',
     storageBucket: 'quickie-23ff0.appspot.com',
@@ -71,8 +71,8 @@ class DefaultFirebaseOptions {
   );
 
   static const FirebaseOptions macos = FirebaseOptions(
-    apiKey: 'AIzaSyB6uQuqKV3Pe-no9xvsl1R6h-0NmUwOMGQ',
-    appId: '1:73653702134:ios:a68a84491c089fb9ad0bd7',
+    apiKey: 'AIzaSyBJ9zuP_j5Joyg_DwGQ',
+    appId: '1:vAIzaSyBJ9zuP_j5Joyg_Dwb9ad0bd7',
     messagingSenderId: '73653702134',
     projectId: 'quickie-23ff0',
     storageBucket: 'quickie-23ff0.appspot.com',
